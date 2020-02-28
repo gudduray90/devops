6284c4ff64cbeba3fe1c888f994112135906d9f2
gudduray89 <rguddu961@gmail.com>
2020-02-28 22:01:41 +0530

    c

---
+++ Playbook/Terraform/create-ec2-instance-1.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    #A provider is responsible for understanding API interactions and exposing resources
+    region  = "us-east-2"  
+}
+
+resource "aws-instance" "my-first-instance" {
+    #Resources define the data types and API interactions required to create, update, and destroy infrastructure with a cloud vendor,
+    # while the Terraform state 
+    #stores mapping and metadata information for those remote objects.
+    ami             = "ami-0998bf58313ab53da"
+    instance_type   = "t2.micro"
+    tags{
+        Name = "Terraform-1"
+    }
+}
+
